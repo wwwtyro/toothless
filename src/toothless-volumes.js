0322e10d2ff9268586cbdde496abf500235e2ac7
rye terrell <ryterrell@ryeterrell.net>
2015-02-16 17:44:43 -0600

    change app data path

--- src/toothless-volumes.js
+++ src/toothless-volumes.js
@@ -2,7 +2,7 @@ var path = require('path');
 var fs = require('fs');
 var rimraf = require('rimraf');
 
-var rootPath = path.join("/opt/toothless");
+var rootPath = path.join("/opt/toothless/app-data");
 var sharedPath = path.join(rootPath, "shared");
 
 // -------------------------------------------------------------
