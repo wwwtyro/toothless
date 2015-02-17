150aaa3ca4b1f2225a04fbebd27bead262d20c43
rye terrell <ryterrell@ryeterrell.net>
2015-02-16 20:19:05 -0600

    add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+if [ ! -e /usr/lib/apt/methods/https ]; then
+    apt-get update
+    apt-get install -y apt-transport-https
+fi
+
+echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+add-apt-repository "deb http://toothless.rocks/ utopic main"
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6CFCE259
+
+apt-get update
+apt-get install -y lxc-docker-1.5.0 
+apt-get install -y toothless
+
+
+
